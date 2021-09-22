c1313cbb3bf187bbf83e9b43ab363ebd0b3d3461
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2021-09-22 11:04:54 +0000

    Learned about arrays and lists, part 2

--- src/ArraysAndLists.dart
+++ src/ArraysAndLists.dart
@@ -1,12 +1,16 @@
 class ArraysAndLists{
 
-  var arr = new List.filled(8, 0, growable: true);
-  var numList = [1,2,3,4,5,6];
+  var arr = new List.filled(8, 5, growable: true);
+  var numList = <int>[];
+
 
 
   void ArraysInDart(){
+    arr[2] = 89;
+    arr.add(45);
+    //arr[8] = 90;
     arr.forEach((element) {
-      print(arr[element]);
+      print(element);
     });
     numList.add(34);
     numList.add(98);
